08a2d382fe2af9a83bac2352c39363931d88e7d9
VeliguraNV <veliguranv@gmail.com>
2024-08-25 17:08:01 +0300

    New tests

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,32 @@
-import static org.junit.Assert.*;
+package com.example;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class CatTest {
-  
+
+    public static final String MEOW = "Мяу";
+
+    @Test
+    public void getSound() {
+        Feline mockFeline = Mockito.mock(Feline.class);
+        Cat cat = new Cat(mockFeline);
+        assertEquals(MEOW, cat.getSound());
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        Feline mockFeline = Mockito.mock(Feline.class);
+        List<String> foodList = List.of("Мясо", "Рыба", "Птица");
+        Mockito.when(mockFeline.eatMeat()).thenReturn(foodList);
+        Cat cat = new Cat(mockFeline);
+        assertEquals(foodList, cat.getFood());
+
+    }
 }
